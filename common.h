e34b7521da75828f11a27721797239a38708b4ca
changwoon <ster72@naver.com>
2015-11-03 21:14:24 +0900

    omok

--- common.h
+++ common.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
 typedef int bit;
 
-/*염색체*/
+/*염색체*/ 
 typedef bit chromosome;
 
 /*개체*/
@@ -15,6 +16,7 @@ typedef struct{
 typedef struct{
 	individual *ivd;
 	int pop_size;
-	int bit_size;
-	int data_count;
-} population;
+	int bit_count;
+	int data_count;	
+} population; 
+
