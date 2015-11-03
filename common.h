1eb5656a635e5888ac7da0af490c6c886eacc3d1
changwoon <ster72@naver.com>
2015-11-03 20:50:54 +0900

    omok

---
+++ common.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef int bit;
+
+/*염색체*/
+typedef bit chromosome;
+
+/*개체*/
+typedef struct{
+	chromosome *crms;
+	double fitness;
+} individual;
+
+/*개체군*/
+typedef struct{
+	individual *ivd;
+	int pop_size;
+	int bit_size;
+	int data_count;
+} population;
