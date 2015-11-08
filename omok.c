9a59a92566df609a0122f41a6cebc20e5b79a879
ster72 <ster72@naver.com>
2015-11-08 20:41:33 +0900

    Update omok.c

--- omok.c
+++ omok.c
@@ -4,6 +4,10 @@ population* create_population(int pop_size, int bit_count, int data_count);
 void free_population(population *pop);
 int rand_between(int a, int b);
 void rand_population(population *pop);
+void evalutate(population *pop, double (*function)(int[], int));
+double fitness_func(int *param, int data_count);
+void mutate(chromosome *crms, int bit_count);
+individual* select_individual(population *pop);
 
 int main(){
 	int i, j;
@@ -11,10 +15,21 @@ int main(){
 	int bit_count = 2;
 	int data_count = 2;
 	population *pop = create_population(pop_size, bit_count, data_count);
+	individual *ivd1, *ivd2;
+	int param[2];
 
 	rand_population(pop);
+	
+	ivd1 = select_individual(pop);
+	ivd2 = select_individual(pop);
+	
+	decode(ivd1->crms, param, bit_count, data_count);
+	decode(ivd2->crms, param, bit_count, data_count);
+	
+	evaluate(pop,fitness_func);
 
 	for(i = 0; i < pop->pop_size; i++){
+		printf("%d번째 fitness : %f\n", i, pop->ivd[i].fitness);
 		for(j = 0; j < bit_count; j++){
 			if(j==2)
 			 printf(" ");
