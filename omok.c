6118b57e6ed1293f89ac07be77838ac35eac2696
ster72 <ster72@naver.com>
2015-11-11 21:36:34 +0900

    Update omok.c

--- omok.c
+++ omok.c
@@ -10,35 +10,38 @@ void mutate(chromosome *crms, int bit_count);
 individual* select_individual(population *pop);
 
 int main(){
-	int i, j;
-	int pop_size = 100;
+	int i, j, x, y, z=0;
+	int pop_size = 1;
 	int bit_count = 2;
 	int data_count = 2;
 	population *pop = create_population(pop_size, bit_count, data_count);
 	individual *ivd1, *ivd2;
 	int param[2];
+	
+	while(z < 255)
+	{
+		printf("x : ");
+		scanf("%d", &x);
+		printf("y : ");
+		scanf("%d", &y);
+		
+		rand_population(pop);//랜덤수 추출 
+	
+		evaluate(pop,fitness_func);
 
-	rand_population(pop);
-	
-	ivd1 = select_individual(pop);
-	ivd2 = select_individual(pop);
-	
-	decode(ivd1->crms, param, bit_count, data_count);
-	decode(ivd2->crms, param, bit_count, data_count);
-	
-	evaluate(pop,fitness_func);
-
-	for(i = 0; i < pop->pop_size; i++){
-		printf("%d번째 fitness : %f\n", i, pop->ivd[i].fitness);
-		for(j = 0; j < bit_count; j++){
-			if(j==2)
+		for(i = 0; i < pop->pop_size; i++){
+		 for(j = 0; j < bit_count; j++){
+			 if(j==8)
 			 printf(" ");
-			printf("%d ", pop->ivd[i].crms[j]);
-		}
+			printf("%d ", pop->ivd[i].crms[j]);//랜덤한 좌표출력 
+			}
 		printf("\n");
-	}
-
+		}	
+		z++;
+	}	
 	free_population(pop);
 	printf("아무키나 누르십시오...");getchar();
 	return 0; 
 }
+
+
