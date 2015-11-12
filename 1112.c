ff71a8cee6df996c11f681663472938ecc47f378
ster72 <ster72@naver.com>
2015-11-12 20:27:59 +0900

    Create 1112.c

---
+++ 1112.c
@@ -0,0 +1,60 @@
+#include "comp2.h"
+
+population* create_population(int pop_size, int bit_count, int data_count);
+void free_population(population *pop);
+int rand_between(int a, int b);
+void rand_population(population *pop);
+void evalutate(population *pop, double (*function)(int[], int));
+double fitness_func(int *param, int data_count);
+void mutate(chromosome *crms, int bit_count);
+individual* select_individual(population *pop);
+
+int main(){
+	int i, j, x, y, z;
+	int pop_size = 1;
+	int bit_count = 2;
+	int data_count = 2;
+	population *pop = create_population(pop_size, bit_count, data_count);
+	individual *ivd1, *ivd2;
+	int param[2];
+	
+	for(z=0;z<255;z++)
+	{
+		printf("x, y 입력\n");
+		scanf("%d %d", &x, &y);
+		
+		if(x<0 || x>14)
+		{
+			printf("다시 입력하십시오\n");
+			continue;
+		}
+		
+		else if(y<0 || y>14)
+		{
+			printf("다시 입력하십시오\n");
+			continue;
+		}
+		
+		rand_population(pop);//랜덤수 추출 
+	
+		evaluate(pop,fitness_func);
+
+		for(i = 0; i < pop->pop_size; i++){
+			printf("컴퓨터의 출력\n");
+		 for(j = 0; j < bit_count; j++){
+			printf("%d ", pop->ivd[i].crms[j]);//랜덤한 좌표출력 
+			}
+		printf("\n");
+		}
+		//*if(x, y == x, y)
+		//{
+		//	printf("다시 입력하십시오\n");
+		//	continue;
+		//}	
+	}	
+	free_population(pop);
+	printf("아무키나 누르십시오...");getchar();
+	return 0; 
+}
+
+
